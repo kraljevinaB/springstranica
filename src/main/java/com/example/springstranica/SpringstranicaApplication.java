377745c3bd72902954fe4cd342e9e9e7ebc6d46b
Marco Bungart <marco.bungart@googlemail.com>
2023-01-07 02:12:18 +0100

    fixed JPA setup

--- src/main/java/com/example/springstranica/SpringstranicaApplication.java
+++ src/main/java/com/example/springstranica/SpringstranicaApplication.java
@@ -2,11 +2,8 @@ package com.example.springstranica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EntityScan("com.example.springstranica.entity")
 public class SpringstranicaApplication {
 
     public static void main(String[] args) {
